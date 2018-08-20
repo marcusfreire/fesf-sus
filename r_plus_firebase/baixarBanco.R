acb06efa5e2a42e736e7a7d2af29ff22574df32f
Marcus Freire <marcusfreire@dcc.ufba.br>
2018-08-20 03:24:50 -0300

    Add Shiny R

--- r_plus_firebase/baixarBanco.R
+++ r_plus_firebase/baixarBanco.R
@@ -6,6 +6,8 @@ if (!require("googleVis")) devtools::install_github("googleVis")
 #Download do Banco sem a autenticação, teste!
 bancoFesf_sus = download(projectURL = "https://fesf-sus.firebaseio.com/", fileName = "/coletas")
 
+cidadesFesf_sus = download(projectURL = "https://fesf-sus.firebaseio.com/", fileName = "/cidades")
+
 #Quantidade de coletas
 #tamanho <- length(bancoFesf_sus)
 
@@ -28,35 +30,82 @@ for (i in 1:length(bancoFesf_sus)){
 
 #Convertendo para o formato Data(date), e factor
 data = as.Date(coletasDATA)
-cidade=as.factor(coletasCIDADE)
+coletasCIDADE=as.factor(coletasCIDADE)
 
 #Adicionando os nomes das colunas dos Inteiros
 colnames(coletasINT) <- names(bancoFesf_sus[[1]])[1:35]
 
 #Criando para dataFrame
-coletasFESF=data.frame(data,cidade,coletasINT)
+coletasFESF=data.frame(data,coletasCIDADE,coletasINT)
 #Verificando o dataFrame
 
+############################# Cidades add
+#Cidades contidas no Firebase
+cidades = c()
+latitude = c()
+longitude = c()
+
+
+#Adicionando os dados na matriz
+for (i in 1:length(cidadesFesf_sus)){
+  cidades[i] = cidadesFesf_sus[[i]]$nome
+  latitude[i] = cidadesFesf_sus[[i]]$latitude
+  longitude[i] = cidadesFesf_sus[[i]]$longitude
+}
+
+#Convertendo para factor
+cidades=as.factor(cidades)
+
+#Frequência das coletas
+freq = rep.int(0,length(cidades))
+freqCidades=data.frame(cidades,freq)
+coletasfreq = table(coletasFESF$coletasCIDADE)
+nomes = labels(coletasfreq)
+for (i in 1:length(coletasfreq)){
+  freqCidades[which(cidades==nomes[[1]][i]),2] = coletasfreq[[i]] 
+}
+
+cidadesFESF=data.frame(cidades,latitude,longitude,freq = freqCidades$freq)
+
+#Soma de todos os indicadores
+
+s = colSums(subset(coletasFESF[,3:37],coletasCIDADE==levels(cidades)[1]))
+soma = data.frame(names(s))
+for (i in 1:length(cidades)){
+  s = colSums(subset(coletasFESF[,3:37],coletasCIDADE==levels(cidades)[i]))
+  soma = cbind(soma,s)
+}
+colnames(soma)=c("Indicadores",levels(cidades))
+#Anotação
+desc = c("Número de gestantes com data provável do parto para o perı́odo atual que são mães solteiras","Número de gestantes com data provável do parto para o perı́odo atual que têm quatro ou mais filhos",
+        "Número de gestantes com data provável do parto para o perı́odo atual que perdeu algum filho com idade menor ou igual a um ano", 
+        "Número de gestantes com data provável do parto para o perı́odo atual que possuem uma renda familiar menor ou igual a um salário mı́nimo",
+        "Número de gestantes com data provável do parto para o perı́odo atual que apresentam quadro de desnutrição ou tem algum filho desnutrido",
+        "Número de gestantes analfabetas com data provável do parto para o perı́odo atual")
 #Subset para a cidade de Salvador
 #subset.data.frame(coletasFESF,coletasFESF$coletasCIDADE=="Salvador")
 # teste com o banco fesf-sus.csv
-fesf = read.csv("fesf-sus.csv",header=T)
-fesf$latlon = paste(fesf$Lat,":",fesf$Long,sep="")
-fesf$rotulo = paste("Cidade: ",fesf$Cidades," - ","Coletas: ",fesf$Freq,sep="")
+#Preparando os dados para visualização
+cidadesFESF$latlon = paste(latitude,":",longitude,sep="")
+cidadesFESF$rotulo = paste("Cidade: ",cidades," - ","Coletas: ",cidadesFESF$freq,sep="")
 
-Map <- gvisMap(fesf,locationvar="latlon",tipvar="rotulo",
+Map <- gvisMap(cidadesFESF,locationvar="latlon",tipvar="rotulo",
       options=list(showTip=TRUE,showLine=TRUE,enableScrollWheel=TRUE,mapType='normal'
       ,useMapTypeControl=TRUE,width="350px",height="450px"))
 plot(Map)
 
-tbl = c(fesf$Cidades,fesf$Freq)
-tabela <- gvisTable(fesf, options=list(page='enable',
-          height='automatic', width='automatic'))
-plot(tabela)
+# Outro exemplo de tabela, descomente para visualizar
+#tbl = c(cidadesFESF$cidades,cidadesFESF$freq)
+#tabela <- gvisTable(cidadesFESF, options=list(page='enable',
+#          height='automatic', width='automatic'))
+#plot(tabela)
 
-tbl = data.frame(fesf$Cidades,fesf$Freq)
-colnames(tbl) = c("Cidades","Coletas")
+tbl = data.frame(cidadesFESF$cidades,cidadesFESF$freq)
+colnames(tbl) = c("Cidades","Nº de Coletas")
 tabela2 <- gvisTable(tbl)
 #Opção de juntas os Gvis
 fesfVis <- gvisMerge(Map, tabela2,horizontal=TRUE)
 plot(fesfVis)
+#TabelaSoma
+tabela2 <- gvisTable(soma[1:6,])
+plot(tabela2)
---
+++ r_plus_firebase/shiny/ui.R
@@ -0,0 +1,89 @@
+# ui.R
+
+dashboardPage(
+  dashboardHeader(title = "FESF-SUS"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Mapa Geral", tabName = "aba1", icon = icon("globe")),
+      menuItem("Socioeconômicos", tabName = "aba2", icon = icon("home")),
+      menuItem("Pré-Parto", tabName = "aba3", icon = icon("info")),
+      menuItem("Parto", tabName = "aba4", icon = icon("users")),
+      menuItem("Intercorrências", tabName = "aba5", icon = icon("bookmark")),
+      menuItem("Boas Práticas", tabName = "aba6", icon = icon("child")),
+      menuItem("Puerpério", tabName = "aba7", icon = icon("clipboard"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "aba1",
+        htmlOutput("maps")
+      ),
+      tabItem(tabName = "aba2",
+        h2("Indicadores Socioeconômicos"),
+        htmlOutput("socio"),
+        htmlOutput("socio2"),
+        h5("A11: Número de gestantes com data provável do parto para o período atual que são mães solteiras."),
+        h5("A12: Número de gestantes com data provável do parto para o perı́odo atual que têm quatro ou mais filhos."),
+        h5("A13: Número de gestantes com data provável do parto para o perı́odo atual que perdeu algum filho com idade menor ou igual a um ano."),
+        h5("A14: Número de gestantes com data provável do parto para o perı́odo atual que possuem uma renda familiar menor ou igual a um salário mı́nimo."),
+        h5("A15: Número de gestantes com data provável do parto para o perı́odo atual que apresentam quadro de desnutrição ou tem algum filho desnutrido."),
+        h5("A16: Número de gestantes analfabetas com data provável do parto para o perı́odo atual.")
+      ),
+      tabItem(tabName = "aba3",
+        h2("Indicadores Pré-Parto"),
+        htmlOutput("pre"),
+        htmlOutput("pre2"),
+        h5("A21: Número de gestantes com data provável do parto para o período atual."),
+        h5("A22: Número de consultas de pré-natal realizadas a partir da 36º semana pelo(a) enfermeiro(a) obstetra."),
+        h5("A23: Número de gestantes que visitaram HPP's no período."),
+        h5("A24: Número de gestantes assistidas no pré-natal com previsão de risco no parto."),
+        h5("A25: Número de atividades na sala de espera realizadas no período."),
+        h5("A26: Número de atividades de roda de conversa com mulheres (gestantes, nutrizes com acompanhante)."),
+        h5("A27: Número de reuniões realizadas com a Atenção Básica para planejamento das ações no território."),
+        h5("A28: Número de oficinas de Educação Permanente realizadas no período.")
+
+      ),
+      tabItem(tabName = "aba4",
+        h2("Indicadores Parto"),
+        htmlOutput("parto"),
+        htmlOutput("parto2"),
+        h5("A31: Número de partos realizados nos HPP's."),
+        h5("A32: Número de partos naturais realizados pelo(a) enfermeiro(a) obstetra no período."),
+        h5("A33: Número de partos naturais realizados e registrados em AIH (Autorização de Internação Hospitalar) pelo(a) enfermeiro(a) obstetra no período."),
+        h5("A34: Número de gestantes encaminhadas com previsão de risco no parto."),
+        h5("A35: Número de natimortos em partos realizados pelo(a) enfermeiro(a) obstetra nos HPP's no período."),
+        h5("A36: Número de nascidos vivos residentes no município.")
+      ),
+      tabItem(tabName = "aba5",
+        h2("Indicadores Intercorrências"),
+        htmlOutput("inter"),
+        htmlOutput("inter2"),
+        h5("A41: Número de episotomias."),
+        h5("A42: Número de lacerações.")
+      ),
+      tabItem(tabName = "aba6",
+        h2("Indicadores Boas Práticas"),
+        htmlOutput("praticas"),
+        htmlOutput("praticas2"),
+        h5("A51: Número de parturientes com acompanhante."),
+        h5("A52: Número de parturientes com Doula, acompanhantes de parto profissionais."),
+        h5("A53: Número de parturientes na posição de parir não supina."),
+        h5("A54: Número de cortes tardios do cordão."),
+        h5("A55: Número de cortes do cordão realizados pelo acompanhante ou pela própria parturiente."),
+        h5("A56: Registro de Apagar no 1º e 5º minuto."),
+        h5("A57: Contato pele a pele na sala de parto."),
+        h5("A58: Número de parturientes que amamentaram em sala de parto.")
+      ),
+      tabItem(tabName = "aba7",
+        h2("Indicadores Puerpério"),
+        htmlOutput("puerp"),
+        htmlOutput("puerp2"),
+        h5("A61: Número de consultas com puerpéras realizadas no mês, conforme necessidade identificada pelo(a) enfermeiro(a) obstetra."),
+        h5("A62: Número de consultas de recém-nascidos realizadas no mês, conforme necessidade identificada pelo(a) enfermeiro(a) obstetra."),
+        h5("A63: Número de consultas realizadas com recém-nascidos que demandaram encaminhamento com pediatra."),
+        h5("A64: Número de recém-nascidos que foram vacinados (BCG e Hepatite B) no HPP."),
+        h5("A65: Agendamento, pelos HPP's, da consulta puerperal na Unidade de Saúde da Família, no momento da alta.")
+      )
+    )
+  )
+)
